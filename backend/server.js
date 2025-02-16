22719e19bc092ff9e92f8662faf9f0370c3406b7
Henrique-Assme <heniassme@gmail.com>
2025-02-16 15:01:54 -0300

    update port for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 connectDB();
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
+    console.log(`Server Started on port ${port}`);
 });
